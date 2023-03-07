c8be9e6d7b9887e410dcabcd25963a293666cd8c
DanielDartDev <olamideonabiyi@student.oauife.edu.ng>
2023-03-07 21:17:03 +0100

    Hero section for desktop and mobilw

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        syne: ['Syne', 'sans-serif'],
+      },
+      colors: {
+        primary: '#5D37A2',
+        secondary: '#0D0D0F',
+        gray: '#C4C4C4',
+      },
+    },
+  },
+  plugins: [],
+};
